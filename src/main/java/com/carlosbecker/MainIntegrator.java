bfafdd52c8a0a3d37ecd479fc4b4291f7e71753e
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-10-15 00:58:14 -0300

    More tests

--- src/main/java/com/carlosbecker/MainIntegrator.java
+++ src/main/java/com/carlosbecker/MainIntegrator.java
@@ -1,11 +1,22 @@
 package com.carlosbecker;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toMap;
+import com.carlosbecker.model.ScriptedRepositories;
+import com.carlosbecker.model.ScriptedRepository;
+import com.carlosbecker.process.ProcessExecutor;
+import com.google.common.collect.Maps;
+import org.eclipse.egit.github.core.Comment;
+import org.eclipse.egit.github.core.PullRequest;
+import org.eclipse.egit.github.core.PullRequestMarker;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.service.IssueService;
+import org.eclipse.egit.github.core.service.PullRequestService;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
@@ -13,16 +24,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import javax.inject.Inject;
 import lombok.extern.log4j.Log4j;
-import org.eclipse.egit.github.core.Comment;
-import org.eclipse.egit.github.core.PullRequest;
-import org.eclipse.egit.github.core.PullRequestMarker;
-import org.eclipse.egit.github.core.Repository;
-import org.eclipse.egit.github.core.service.IssueService;
-import org.eclipse.egit.github.core.service.PullRequestService;
-import com.carlosbecker.model.ScriptedRepositories;
-import com.carlosbecker.model.ScriptedRepository;
-import com.carlosbecker.process.ProcessExecutor;
-import com.google.common.collect.Maps;
 
 @Log4j
 public class MainIntegrator {
@@ -102,10 +103,11 @@ public class MainIntegrator {
     private List<String> buildParamList(PullRequest pr, List<String> params) {
         final PullRequestMarker head = pr.getHead();
         final Repository headRepo = head.getRepo();
-        final List<String> parsedParams = asList(headRepo.getOwner().getLogin(),
+        final List<String> parsedParams = newArrayList();
+        parsedParams.addAll(asList(headRepo.getOwner().getLogin(),
                 headRepo.getName(),
                 head.getRef(),
-                "" + pr.getNumber());
+                "" + pr.getNumber()));
         parsedParams.addAll(params);
         return parsedParams;
     }
